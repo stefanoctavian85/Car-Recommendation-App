ef8398cfeb3dd37e7e2591f89bf1027724ca8bdf
stefanoctavian85 <stefanoctavian85@gmail.com>
2025-02-19 11:13:40 +0200

    first commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server has started on PORT ${PORT}`);
+})
