6bfcf41436cc0962cfb26dd7189deb5c93755ace
stefanoctavian85 <stefanoctavian85@gmail.com>
2025-02-20 14:02:47 +0200

    Added login component

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import database from './models/index.js';
 import routers from './routers/index.js'
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 dotenv.config();
 
 database.connectToDatabase();
---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,58 @@
+import './Login.css';
+import React, { useState } from 'react';
+import { SERVER } from '../../config/global.js';
+
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function login() {
+        const userCredentials = {
+            email, password,
+        }
+        const response = await fetch(`${SERVER}/auth/login`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userCredentials)
+        })
+        
+        const data = await response.json();
+        console.log(data);
+    }
+
+    return (
+        <div className='login-page'>
+            <h2>Welcome back!</h2>
+            <p>Login to access your account!</p>
+            <form className='login-form'>
+                <div className='login-input'>
+                    <label htmlFor='email-input'>Email</label>
+                    <input
+                        id='email-input'
+                        type='text'
+                        placeholder='Enter your email...'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className='login-form'>
+                    <label htmlFor='password-input'>Password</label>
+                    <input
+                        id='password-input'
+                        type='password'
+                        placeholder='Enter your password...'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button className='login-button' type='button' onClick={login}>Log in</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
