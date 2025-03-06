f3213bf45c87576ff0fb92f8a05adab51cf4d533
stefanoctavian85 <stefanoctavian85@gmail.com>
2025-03-06 17:20:31 +0200

    Styled register component

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -3,6 +3,13 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { SERVER } from '../../config/global.jsx';
 import AppContext from '../../state/AppContext.jsx';
+import { Box, Container, Typography, Button, Avatar, FormControl, Input, InputAdornment, IconButton, InputLabel, FormHelperText } from '@mui/material';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import SendIcon from '@mui/icons-material/Send';
+import ErrorIcon from '@mui/icons-material/Error';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 
 function Register() {
     const { auth } = useContext(AppContext);
@@ -11,39 +18,143 @@ function Register() {
     const [lastname, setLastname] = useState('');
     const [password, setPassword] = useState("");
     const [error, setError] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const [emailError, setEmailError] = useState('');
+    const [firstnameError, setFirstnameError] = useState('');
+    const [lastnameError, setLastnameError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
+    const [emailTouched, setEmailTouched] = useState(false);
+    const [firstnameTouched, setFirstnameTouched] = useState(false);
+    const [lastnameTouched, setLastnameTouched] = useState(false);
+    const [passwordTouched, setPasswordTouched] = useState(false);
+
     const navigate = useNavigate();
 
     useEffect(() => {
-            if (auth.isAuthenticated) {
-                navigate('/profile');
-            }
-        }, [auth.isAuthenticated]);
+        if (auth.isAuthenticated) {
+            navigate('/profile');
+        }
+    }, [auth.isAuthenticated]);
 
-    function validateCredentials() {
+    function handleShowPassword() {
+        setShowPassword(!showPassword);
+    }
+
+    function validateEmail(email) {
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-        if (!emailRegex.test(email)) {
-            setError("Invalid email address!");
+        if (!email) {
+            setEmailError("Email is required!");
             return false;
-        }
-
-        if (firstname.length < 3 || firstname.length > 20) {
-            setError("First name length must be between 3 and 20!");
+        } else if (!emailRegex.test(email)) {
+            setEmailError("Invalid email address!");
             return false;
+        } else {
+            setEmailError("");
+            return true;
         }
+    }
 
-        if (lastname.length < 3 || lastname.length > 20) {
-            setError("Last name length must be between 3 and 20!");
+    function validateFirstname(firstname) {
+        if (!firstname) {
+            setFirstnameError("First name is required!");
             return false;
-        }
-
-        if (password.length < 5 || password.length > 64) {
-            setError("Password length must be between 5 and 64!");
+        } else if (firstname.length < 3 || firstname.length > 20) {
+            setFirstnameError("First name length must be between 3 and 20!");
             return false;
+        } else {
+            setFirstnameError("");
+            return true;
         }
+    }
 
-        setError('');
-        return true;
+    function validateLastname(lastname) {
+        if (!lastname) {
+            setLastnameError("Last name is required!");
+            return false;
+        } else if (lastname.length < 3 || lastname.length > 20) {
+            setLastnameError("Last name length must be between 3 and 20!");
+            return false;
+        } else {
+            setLastnameError("");
+            return true;
+        }
+    }
+
+    function validatePassword(password) {
+        if (!password) {
+            setPasswordError("Password is required!");
+            return false;
+        } else if (password.length < 5 || password.length > 64) {
+            setPasswordError("Password length must be between 5 and 64!");
+            return false;
+        } else {
+            setPasswordError("");
+            return true;
+        }
+    }
+
+    function handleEmailChange(e) {
+        const email = e.target.value;
+        setEmail(email);
+        if (emailTouched) {
+            validateEmail(email);
+        }
+    }
+
+    function handleFirstnameChange(e) {
+        const firstname = e.target.value;
+        setFirstname(firstname);
+        if (firstnameTouched) {
+            validateFirstname(firstname);
+        }
+    }
+
+    function handleLastnameChange(e) {
+        const lastname = e.target.value;
+        setLastname(lastname);
+        if (lastnameTouched) {
+            validateLastname(lastname);
+        }
+    }
+
+    function handlePasswordChange(e) {
+        const password = e.target.value;
+        setPassword(password);
+        if (passwordTouched) {
+            validatePassword(password);
+        }
+    }
+
+    function handleEmailLive(e) {
+        setEmailTouched(true);
+        validateEmail(e.target.value);
+    }
+
+    function handleFirstnameLive(e) {
+        setFirstnameTouched(true);
+        validateFirstname(e.target.value);
+    }
+
+    function handleLastnameLive(e) {
+        setLastnameTouched(true);
+        validateLastname(e.target.value);
+    }
+
+    function handlePasswordLive(e) {
+        setPasswordTouched(true);
+        validatePassword(e.target.value);
+    }
+
+    function validateCredentials() {
+        const isEmailValid = validateEmail(email);
+        const isFirstnameValid = validateFirstname(firstname);
+        const isLastnameValid = validateLastname(lastname);
+        const isPasswordValid = validatePassword(password);
+
+        return isEmailValid && isFirstnameValid && isLastnameValid && isPasswordValid;
     }
 
     async function register() {
@@ -76,62 +187,147 @@ function Register() {
     }
 
     return (
-        <div className='register-page'>
-            <h2>Create a new account!</h2>
-            <p>Fill in the fields</p>
-            <form className='form-register'>
-                <div className='register-input'>
-                    <label htmlFor='email-input'>Email</label>
-                    <input
+        <Container maxWidth="sx">
+            <Box className="register-title">
+                <Avatar className='register-icon'>
+                    <HowToRegIcon></HowToRegIcon>
+                </Avatar>
+                <Typography component="h1">Create a new account</Typography>
+            </Box>
+            <Box className='register-form' component='form'>
+                <FormControl className='register-input' error={!!emailError}>
+                    <InputLabel htmlFor='email-input'>Email</InputLabel>
+                    <Input
                         id='email-input'
-                        type='email'
-                        placeholder='Enter your email...'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        onChange={handleEmailChange}
+                        onBlur={handleEmailLive}
+                        label="Email"
+                        type='text'
                         required
-                    />
-                </div>
-                <div className='register-input'>
-                    <label htmlFor='firstname-input'>First name</label>
-                    <input
+                        endAdornment={
+                            emailTouched && (
+                                <InputAdornment position='end'>
+                                    {
+                                        emailError ? (
+                                            <ErrorIcon color='error' />
+                                        ) :
+                                            <CheckCircleIcon color='success' />
+                                    }
+                                </InputAdornment>
+                            )
+                        }
+                    >
+                    </Input>
+                    {emailError ? (<FormHelperText error>{emailError}</FormHelperText>) : null}
+                </FormControl>
+            </Box>
+            <Box className='register-form' component='form'>
+                <FormControl className='register-input' error={!!firstnameError}>
+                    <InputLabel htmlFor='firstname-input'>First name</InputLabel>
+                    <Input
                         id='firstname-input'
+                        onChange={handleFirstnameChange}
+                        onBlur={handleFirstnameLive}
+                        label="First name"
                         type='text'
-                        placeholder='Enter your first name...'
-                        minLength="3"
-                        maxLength="20"
-                        value={firstname}
-                        onChange={(e) => setFirstname(e.target.value)}
                         required
-                    />
-                </div>
-                <div className='register-input'>
-                    <label htmlFor='lastname-input'>Last name</label>
-                    <input
+                        endAdornment={
+                            firstname && (
+                                <InputAdornment position='end'>
+                                    {
+                                        firstnameError ? (
+                                            <ErrorIcon color='error' />
+                                        ) :
+                                            <CheckCircleIcon color='success' />
+                                    }
+                                </InputAdornment>
+                            )
+                        }
+                    >
+                    </Input>
+                    {firstnameError ? (<FormHelperText error>{firstnameError}</FormHelperText>) : null}
+                </FormControl>
+            </Box>
+            <Box className='register-form' component='form'>
+                <FormControl className='register-input' error={!!lastnameError}>
+                    <InputLabel htmlFor='lastname-input'>Last name</InputLabel>
+                    <Input
                         id='lastname-input'
+                        onChange={handleLastnameChange}
+                        onBlur={handleLastnameLive}
+                        label="Last name"
                         type='text'
-                        placeholder='Enter your last name...'
-                        minLength="3"
-                        maxLength="20"
-                        value={lastname}
-                        onChange={(e) => setLastname(e.target.value)}
                         required
-                    />
-                </div>
-                <div className='register-input'>
-                    <label htmlFor='password-input'>Password</label>
-                    <input
+                        endAdornment={
+                            lastnameTouched && (
+                                <InputAdornment position='end'>
+                                    {
+                                        lastnameError ? (
+                                            <ErrorIcon color='error' />
+                                        ) :
+                                            <CheckCircleIcon color='success' />
+                                    }
+                                </InputAdornment>
+                            )
+                        }
+                    >
+                    </Input>
+                    {lastnameError ? (<FormHelperText error>{lastnameError}</FormHelperText>) : null}
+                </FormControl>
+            </Box>
+            <Box className='register-form' component='form'>
+                <FormControl className='register-input' error={!!passwordError}>
+                    <InputLabel htmlFor='password-input'>Password</InputLabel>
+                    <Input
                         id='password-input'
-                        type='password'
-                        placeholder='Enter your password...'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        onChange={handlePasswordChange}
+                        onBlur={handlePasswordLive}
+                        label="Password"
+                        type={showPassword ? 'text' : 'password'}
                         required
-                    />
-                </div>
-                <button className='register-button' type='button' onClick={register}>Register</button>
-            </form>
-            <p className='register-error'>{error}</p>
-        </div>
+                        endAdornment={
+                            <InputAdornment position='end'>
+                                {
+                                    passwordTouched && (
+                                        <>
+                                            {
+                                                passwordError ? (
+                                                    <ErrorIcon color='error' />
+                                                ) : password ? (
+                                                    <CheckCircleIcon color='success' />
+                                                ) : null}
+                                        </>
+                                    )
+                                }
+                                <IconButton
+                                    aria-label={
+                                        showPassword ? 'hide the password' : 'display the password'
+                                    }
+                                    onClick={handleShowPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    >
+                    </Input>
+                    {passwordError ? (<FormHelperText error>{passwordError}</FormHelperText>) : null}
+                </FormControl>
+            </Box>
+            <Box className='register-button'>
+                <Button
+                    variant='contained'
+                    endIcon={<SendIcon />}
+                    onClick={register}
+                >
+                    Sign Up
+                </Button>
+            </Box>
+            <Box className='register-error'>
+                {error ? (<Typography>{error}</Typography>) : null}
+            </Box>
+        </Container>
     );
 }
 
