ed0b8e7e49760cd4236a72d9e2db2f6dfe79295f
stefanoctavian85 <stefanoctavian85@gmail.com>
2025-03-08 10:01:31 +0200

    Changed scraper to extract images and equipment details for cars

--- carPredict/car_scraper/cars_scraper.py
+++ carPredict/car_scraper/cars_scraper.py
@@ -13,101 +13,114 @@ load_dotenv(dotenv_path=".env")
 path = os.getenv("CHROMEDRIVER_PATH")
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
+options.add_argument("--headless=new")
+options.add_argument('--incognito')
+options.add_argument("start-maximized")
+options.add_argument("disable-infobars")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-application-cache')
+options.page_load_strategy = 'eager'
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 service = Service(path)
 
 driver = webdriver.Chrome(service=service, options=options)
 driver.get(website)
 
 time.sleep(1)
-accept_cookies_button = driver.find_element(By.ID, "onetrust-accept-btn-handler")
-accept_cookies_button.click()
+
+accept_button = driver.find_element(By.ID, "onetrust-accept-btn-handler")
+accept_button.click()
 
 data_links = []
-number = 0
-page_number = 0
 
-car_data_list = []
-
-while True:
-    try:
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "article.ooa-16cop2i.e1oaf45v0")))
-        cars_paragraphs = driver.find_elements(By.CSS_SELECTOR, "article.ooa-16cop2i.e1oaf45v0")
-
-        for car in cars_paragraphs:
-            link = car.find_element(By.TAG_NAME, "a").get_attribute("href")
-            number = number + 1
-            print(str(number) + " " + link)
-            data_links.append(link)
-
+def extract_car_links():
+    page_number = 0
+    while True:
         try:
-            next_page_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//li[@title="Go to next Page"]')))
-            if "disabled" in next_page_button.get_attribute("class"):
+            cars_paragraphs = driver.find_elements(By.CSS_SELECTOR, "article.ooa-16cop2i.e1oaf45v0")
+
+            for car in cars_paragraphs:
+                link = car.find_element(By.TAG_NAME, "a").get_attribute("href")
+                data_links.append(link)
+
+            try:
+                next_page_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//li[@title="Go to next Page"]')))
+                if "disabled" in next_page_button.get_attribute("class"):
+                    break
+
+                driver.execute_script("arguments[0].click();", next_page_button)
+                page_number = page_number + 1
+                print("Page number" + str(page_number))
+            except Exception:
+                print("No more pages!")
                 break
-
-            driver.execute_script("arguments[0].click();", next_page_button)
-            page_number = page_number + 1
-            print("Page number" + str(page_number))
-            time.sleep(2)
-        except Exception as e:
-            print("No more pages!")
+        except Exception:
             break
-    except Exception as e:
-        break
+    
+    return data_links
 
-number = 0
-
-for link in data_links:
-    try:
-        driver.get(link)
-        car_info = {}
-
+def extract_car_details(data_links):
+    number = 0
+    car_data_list = []
+    for link in data_links:
         try:
-            image = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.ooa-12np8kw.e142atj30")))
-            image_link = image.find_element(By.TAG_NAME, "img").get_attribute("srcset")
-            car_info["Imagine"] = image_link
-        except Exception as e:
-            print("Image not found!")
+            driver.get(link)
+            car_info = {}
 
-        technical_specs_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "header.ooa-1g1u77j")))
-        technical_specs_button.click()
+            technical_specs_button = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "header.ooa-1g1u77j")))
+            technical_specs_button.click()
 
-        pret = driver.find_element(By.CSS_SELECTOR, "h3.offer-price__number.ex6ng1i5.ooa-1kdys7g")
-        moneda = driver.find_element(By.CSS_SELECTOR, "p.offer-price__currency.ex6ng1i6.ooa-m6bn4u")
-        car_info["Pret"] = pret.text.strip() + moneda.text.strip()
+            try:
+                tags = WebDriverWait(driver, 3).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "p.en2sar58.ooa-y26jp")))
+                details = WebDriverWait(driver, 3).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "p.en2sar59.ooa-17xeqrd")))
 
-        try:
-            tags = WebDriverWait(driver, 5).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "p.en2sar58.ooa-y26jp")))
-            details = WebDriverWait(driver, 5).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "p.en2sar59.ooa-17xeqrd")))
+                pozitie = 0
 
-            pozitie = 0
+                for tag in tags:
+                    eticheta = tag.text.strip()
 
-            for tag in tags:
-                eticheta = tag.text.strip()
+                    if eticheta == "VIN (serie sasiu)":
+                        continue
 
-                if eticheta == "VIN (serie sasiu)":
-                    continue
+                    if pozitie < len(details):
+                        car_info[eticheta] = details[pozitie].text.strip()
+                        pozitie += 1
 
-                if pozitie < len(details):
-                    car_info[eticheta] = details[pozitie].text.strip()
-                    pozitie += 1
+                car_info["KM"] = driver.find_element(By.CSS_SELECTOR, "p.e1btp7412.ooa-1rcllto").text.strip()
 
-            print(f"Car number {number}")
-            for info in car_info:
-                print(info + ": " + car_info[info])
-            number += 1
+                pret = driver.find_element(By.CSS_SELECTOR, "h3.offer-price__number.ex6ng1i5.ooa-1kdys7g")
+                moneda = driver.find_element(By.CSS_SELECTOR, "p.offer-price__currency.ex6ng1i6.ooa-m6bn4u")
+                car_info["Pret"] = pret.text.strip() + moneda.text.strip()
 
-            car_data_list.append(car_info)
-        except Exception as e:
-            print("No specification for this car")
+                try:
+                    equipments = WebDriverWait(driver, 3).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "p.eunhvho3.ooa-1afepcx")))
+                    car_info["Dotari"] = [equip.text.strip() for equip in equipments]
+                except Exception:
+                    print("Equipment not found!")
 
-    except Exception as e:
-        print(e)
+                try:
+                    images = WebDriverWait(driver, 5).until(
+                        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.embla__slide.ooa-4g6ai3.e142atj32 img")))
+                    car_info["Imagine"] = ', '.join(img.get_attribute("srcset").split(',')[0].split(" ")[0] for img in images)
+                except Exception:
+                    print("Image not found!")
 
-car_data_df = pd.DataFrame(car_data_list)
-car_data_df = car_data_df.rename(columns={'Anul producției': 'Anul productiei'})
-print(car_data_df)
-columns = list(car_data_df.columns)
-car_data_df.to_csv("raw/test.csv", index=False, columns=columns)
+                print(f"Car number {number}")
+                number = number + 1
+                car_data_list.append(car_info)
+            except Exception as e:
+                print("No specification for this car", e)
+
+        except Exception:
+            print("Eroare")
+    return car_data_list
+
+data_links = extract_car_links()
+data_links_df = pd.DataFrame(data_links, columns=['Link'])
+data_links_df.index.name = "Masina"
+data_links_df.to_csv("raw/cars_links.csv")
+
+data_links_df = pd.read_csv("raw/cars_links.csv", index_col=0)
+cars = extract_car_details(data_links_df["Link"].tolist())
+car_data_df = pd.DataFrame(cars)
+car_data_df.to_csv("raw/cars_dataset.csv", index=False)
